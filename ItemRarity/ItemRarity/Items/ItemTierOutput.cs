3e9d5faec2a6b5ac694a580f258f682934c16029
Dorian <dodo-pl-08@hotmail.fr>
2026-04-24 15:46:34 +0200

    Add null check for itemstack

--- ItemRarity/ItemRarity/Items/ItemTierOutput.cs
+++ ItemRarity/ItemRarity/Items/ItemTierOutput.cs
@@ -1,18 +1,25 @@
 ﻿using System.Linq;
+using ItemRarity.Logging;
 using Vintagestory.API.Common;
 
 namespace ItemRarity.Items;
 
 public sealed class ItemTierOutput : Item
 {
-    public override void OnCreatedByCrafting(ItemSlot[] allInputslots, ItemSlot outputSlot, GridRecipe byRecipe)
+    public override void OnCreatedByCrafting(ItemSlot[] allInputSlots, ItemSlot outputSlot, IRecipeBase byRecipe)
     {
-        var tierItem = allInputslots.FirstOrDefault(s => s.Itemstack?.Collectible?.Code.PathStartsWith("tier") ?? false);
-        var targetItem = allInputslots.FirstOrDefault(s => s.Itemstack?.Collectible?.Durability > 1);
+        var tierItem = allInputSlots.FirstOrDefault(s => s.Itemstack?.Collectible?.Code.PathStartsWith("tier") ?? false);
+        var targetItem = allInputSlots.FirstOrDefault(s => s.Itemstack?.Collectible?.Durability > 1);
 
         if (tierItem == null || targetItem == null)
         {
             outputSlot.Itemstack = null;
+            return;
+        }
+
+        if (targetItem.Itemstack is null)
+        {
+            Logger.Error("targetItem.Itemstack is null");
             return;
         }
 
