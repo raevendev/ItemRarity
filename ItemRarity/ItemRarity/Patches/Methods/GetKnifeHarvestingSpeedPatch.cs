55d77179d644e8a689f80e068e9fde4864c016d2
Dorian <dodo-pl-08@hotmail.fr>
2026-04-25 01:32:04 +0200

    Support knife and remove old patch

---
+++ ItemRarity/ItemRarity/Patches/Methods/GetKnifeHarvestingSpeedPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using ItemRarity.Attributes;
+using ItemRarity.Rarities;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+// ReSharper disable InconsistentNaming
+
+namespace ItemRarity.Patches.Methods;
+
+[HarmonyPatch]
+public static class GetKnifeHarvestingSpeedPatch
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(ItemKnife), nameof(ItemKnife.GetKnifeHarvestingSpeed)), HarmonyPriority(Priority.Last)]
+    public static void GetKnifeHarvestingSpeed(ItemKnife __instance, ItemSlot slot, ref float __result)
+    {
+        if (!Rarity.TryGetRarity(slot.Itemstack, out _))
+            return;
+        
+        __result = (float)(1.0 / (((__instance.GetMiningSpeeds(slot)[EnumBlockMaterial.Plant] * Attribute.MiningSpeedMultiplier.GetFloat(slot.Itemstack)) - 1.0) * 0.5 +
+                                  1.0));
+        
+    }
+}
