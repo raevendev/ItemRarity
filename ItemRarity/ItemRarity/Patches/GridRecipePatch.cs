ad62780007c6e7b30258c36806ad0e66f02a0aaa
Dorian <dodo-pl-08@hotmail.fr>
2026-04-24 15:42:37 +0200

    Fix crafting patch for tiers

--- ItemRarity/ItemRarity/Patches/GridRecipePatch.cs
+++ ItemRarity/ItemRarity/Patches/GridRecipePatch.cs
@@ -7,19 +7,16 @@ using Vintagestory.API.Common;
 
 namespace ItemRarity.Patches;
 
-[HarmonyPatch(typeof(GridRecipe))]
+[HarmonyPatch(typeof(RecipeBase))]
 public static class GridRecipePatch
 {
     [HarmonyPrefix, HarmonyPatch("MatchesShapeLess"), HarmonyPriority(Priority.Last)]
-    public static bool MatchesShapeLessPatch(GridRecipe __instance, ref bool __result, ItemSlot[] suppliedSlots, int gridWidth)
+    public static bool MatchesShapeLessPatch(RecipeBase __instance, ref bool __result, ItemSlot[] suppliedSlots, IWorldAccessor world,
+        IRecipeIngredient?[] ingredients)
     {
         if (!ModCore.Config.Tier.EnableTiers || !__instance.Name.Path.Contains("tier-recipe-output"))
             return true;
-
-        var num = suppliedSlots.Length / gridWidth;
-        if (gridWidth < __instance.Width || num < __instance.Height)
-            return false;
-
+        
         // Filter to non-empty slots
         var nonEmptySlots = suppliedSlots.Where(s => s.Itemstack?.Collectible != null).ToArray();
         if (nonEmptySlots.Length != 2)
--- ItemRarity/ItemRarity/Patches/Methods/ConsumeCraftingIngredientsPatch.cs
+++ ItemRarity/ItemRarity/Patches/Methods/ConsumeCraftingIngredientsPatch.cs
@@ -14,19 +14,40 @@ namespace ItemRarity.Patches.Methods;
 [HarmonyPatch]
 public static class ConsumeCraftingIngredientsPatch
 {
+    /// <summary>
+    /// Overrides the ingredient consumption logic for general collectible objects during crafting.
+    /// Replaces the original result with a custom implementation.
+    /// </summary>
+    /// <param name="__instance">The collectible object instance involved in the crafting.</param>
+    /// <param name="slots">The input item slots containing the ingredients.</param>
+    /// <param name="outputSlot">The output slot where the crafted item will be placed.</param>
+    /// <param name="matchingRecipe">The recipe being executed.</param>
+    /// <param name="__result">The final success status of the ingredient consumption.</param>
     [HarmonyPatch(typeof(CollectibleObject), nameof(CollectibleObject.ConsumeCraftingIngredients)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
-    public static void CollectibleObject_ConsumeCraftingIngredientsPatch(CollectibleObject __instance, ItemSlot[] slots, ItemSlot outputSlot, GridRecipe matchingRecipe, ref bool __result)
+    public static void CollectibleObject_ConsumeCraftingIngredientsPatch(CollectibleObject __instance, ItemSlot[] slots, ItemSlot outputSlot, GridRecipe matchingRecipe,
+        ref bool __result)
     {
-       __result = ConsumeCraftingIngredients(slots, outputSlot, matchingRecipe);
+        __result = ConsumeCraftingIngredients(slots, outputSlot, matchingRecipe);
     }
 
-    [HarmonyPatch(typeof(ItemWearable), nameof(ItemWearable.ConsumeCraftingIngredients)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
-    public static void ItemWearable_ConsumeCraftingIngredientsPatch(ItemWearable __instance, ItemSlot[] inSlots, ItemSlot outputSlot, GridRecipe recipe, ref bool __result)
+    /// <summary>
+    /// Overrides the ingredient consumption logic for wearable items during crafting.
+    /// Replaces the original result with a custom implementation.
+    /// </summary>
+    /// <param name="__instance">The wearable item instance involved in the crafting.</param>
+    /// <param name="inSlots">The input item slots containing the ingredients.</param>
+    /// <param name="outputSlot">The output slot where the crafted item will be placed.</param>
+    /// <param name="recipe">The recipe being executed.</param>
+    /// <param name="bhHandling">The event handling status (modified by reference).</param>
+    /// <param name="__result">The final success status of the ingredient consumption.</param>
+    [HarmonyPatch(typeof(CollectibleBehaviorWearable), nameof(CollectibleBehaviorWearable.ConsumeCraftingIngredients)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
+    public static void CollectibleBehaviorWearable_ConsumeCraftingIngredientsPatch(CollectibleBehaviorWearable __instance, ItemSlot[] inSlots, ItemSlot outputSlot, IRecipeBase recipe,
+        ref EnumHandling bhHandling, ref bool __result)
     {
         __result = ConsumeCraftingIngredients(inSlots, outputSlot, recipe);
     }
 
-    private static bool ConsumeCraftingIngredients(ItemSlot[] inSlots, ItemSlot outputSlot, GridRecipe recipe)
+    private static bool ConsumeCraftingIngredients(ItemSlot[] inSlots, ItemSlot outputSlot, IRecipeBase recipe)
     {
         if (outputSlot is not { Itemstack: not null })
             return false;
@@ -58,7 +79,7 @@ public static class ConsumeCraftingIngredientsPatch
                     Tier.ApplyTierUpgrade(targetItem.Itemstack, outputSlot.Itemstack, tierItem.Itemstack.Collectible.Code.EndVariantInteger());
                 else
                     Tier.ApplyTier(outputSlot.Itemstack, tierItem.Itemstack.Collectible.Code.EndVariantInteger());
-                
+
                 foreach (var slot in inSlots.Where(s => s.Itemstack?.Collectible?.Code == outputSlot.Itemstack?.Collectible?.Code))
                     slot.TakeOut(1);
                 break;
